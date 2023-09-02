ea708d48043aec2dba865ea90d9d3364abda5172
ZYW <ZYW@DESKTOP-9CJ44G3>
2023-09-03 01:33:24 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AutoName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::AutoName.Properties.Resources._64;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(170, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 57);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("等线", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(170, 124);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(250, 58);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "名称";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::AutoName.Properties.Resources.backgroundtop;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::AutoName.Properties.Resources.close;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(548, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(517, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "抽人Ver.3.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "全自动抽人软件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AutoName.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection.Emit;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Runtime.InteropServices;
+using NPinyin;
+using System.Media;
+using System.Diagnostics;
+
+
+namespace AutoName
+{
+    public partial class Form1 : Form
+
+    {
+        private List<string> names = new List<string>();
+        private Point mouseOffset;
+        private bool isMouseDown = false;
+
+        public Form1()
+        {
+            // 从文本文件中读取名字
+            ReadNamesFromFile("names.txt");
+            // 初始化随机数种子
+            InitializeComponent();
+
+
+        }
+
+        private void ReadNamesFromFile(string filename) //读取文件
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filename);
+                names.AddRange(lines);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"无法打开文件 {filename}：{ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+
+        }
+
+        private void PickAndDisplayRandomName() //抽取
+        {
+            // 检查是否有可用的名字
+            if (names.Count == 0)
+            {
+                MessageBox.Show("没有找到名字", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 生成随机索引
+            Random rand = new Random();
+            int randomIndex = rand.Next(names.Count);
+            // 将名字显示在 Label 控件中
+            label1.Text = names[randomIndex];
+
+
+            string name = names[randomIndex];
+            string piny = Pinyin.GetPinyin(name);
+            string outputDirectory = "sources";
+
+
+            //播放名字音频
+            foreach (char character in name)
+            {
+                string pinyin = Pinyin.GetPinyin(character.ToString());
+                string fileName = $"{pinyin}.wav";
+                string filePath = Path.Combine(outputDirectory, fileName);
+
+                // 在这里需要根据实际情况获取对应的音频文件路径
+                // 例如，如果使用文件名匹配的方式，可以根据 pinyin 获取对应的音频文件
+
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        using (SoundPlayer player = new SoundPlayer(filePath))
+                        {
+                            player.PlaySync(); // 播放声音
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error playing sound: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Audio file not found for: {character}");
+                }
+            }
+
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public class KeyboardHook
+        {
+            private const int WH_KEYBOARD_LL = 13;
+            private const int WM_KEYDOWN = 0x0100;
+            private const int VK_C = 0x43;
+
+            private IntPtr hookId = IntPtr.Zero;
+            private LowLevelKeyboardProc hookCallback;
+
+            public event EventHandler CKeyPressed;
+
+            public void Start()
+            {
+                hookCallback = HookCallback;
+                using (Process process = Process.GetCurrentProcess())
+                using (ProcessModule module = process.MainModule)
+                {
+                    IntPtr hModule = GetModuleHandle(module.ModuleName);
+                    hookId = SetWindowsHookEx(WH_KEYBOARD_LL, hookCallback, hModule, 0);
+                }
+            }
+
+            public void Stop()
+            {
+                UnhookWindowsHookEx(hookId);
+            }
+
+            private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+            {
+                if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    if (vkCode == VK_C)
+                    {
+                        CKeyPressed?.Invoke(this, EventArgs.Empty);
+                    }
+                }
+
+                return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+            }
+
+            private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string lpModuleName);
+        }
+
+        public class ProgramWindow
+        {
+            [DllImport("user32.dll")]
+            private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+            private const int SW_SHOW = 5;
+
+            public void Show()
+            {
+                var handle = Process.GetCurrentProcess().MainWindowHandle;
+                ShowWindow(handle, SW_SHOW);
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // 记录鼠标按下时的位置和PictureBox的位置
+            if (e.Button == MouseButtons.Left)
+            {
+                int xOffset = -e.X - SystemInformation.FrameBorderSize.Width;
+                int yOffset = -e.Y - SystemInformation.CaptionHeight -
+                    SystemInformation.FrameBorderSize.Height;
+                mouseOffset = new Point(xOffset, yOffset);
+                isMouseDown = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // 移动窗口和PictureBox
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // 清除记录的位置信息
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PickAndDisplayRandomName();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
